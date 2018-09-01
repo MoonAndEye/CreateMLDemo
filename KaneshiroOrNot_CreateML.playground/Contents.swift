dce0fda97f10f9ead87ef75936bd67fb0adcb6cb
MoonAndEye <atimis19@gmail.com>
2018-09-01 21:27:08 +0800

    1. Modify Keynote, for ML framework 2. Add fruit dataset 3. Add Kaneshiro dataset

--- KaneshiroOrNot_CreateML.playground/Contents.swift
+++ KaneshiroOrNot_CreateML.playground/Contents.swift
@@ -1,16 +1,9 @@
 import CreateMLUI
-import UIKit
-if #available(OSX 10.14, *) {
-    let builder = MLImageClassifierBuilder()
-    builder.showInLiveView()
-    print(builder)
-} else {
-    // Fallback on earlier versions
-}
-
-
-//import CreateML
 import Foundation
+
+let builder = MLImageClassifierBuilder()
+builder.showInLiveView()
+
 //
 //let data = try MLDataTable(contentsOf: URL(fileURLWithPath: "<#/path/to/read/data.json#>"))
 //
